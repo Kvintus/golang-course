5c1ecfdaef6642bbcc0b262bf64bfbbe9f50eb2b
Jakob Rolik <rolik.jakob@gmail.com>
2019-05-26 12:51:13 +0200

    Added file manipulation

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -12,15 +17,39 @@ func newDeck() deck {
 
 	for _, cardSuit := range cardSuits {
 		for _, cardValue := range cardValues {
-			cards = append(cards, cardValue + " of " + cardSuit)
+			cards = append(cards, cardValue+" of "+cardSuit)
 		}
 	}
 
 	return cards
 }
 
+func newDeckFromFile(fileName string) deck {
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ",err)
+		os.Exit(1)
+	}
+
+	fileStringContent := string(contents)
+	stringSlice := strings.Split(fileStringContent, ",")
+	return deck(stringSlice)
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
 func (deck deck) print() {
 	for i, card := range deck {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,8 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.print()
+	//cards := newDeck()
+	//newHand, remainingCards := deal(cards, 5)
+	//newHand.print()
+	//remainingCards.print()
+	//fmt.Println(remainingCards.toString())
+	fromFile := newDeckFromFile("test.txt")
+	fromFile.print()
 }
 
 func newCard() string {
